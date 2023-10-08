ce1d85ffd1e7bc7ab2e6e6f191689cdb2db2edcf
takato hori <iuyda@icloud.com>
2023-10-08 17:02:39 +0900

    Add user from components

--- pages/signup.vue
+++ pages/signup.vue
@@ -1,12 +1,26 @@
 <template>
-  <div>
-    signup.vue
-  </div>
+  <user-form-card>
+    <template #user-form-card-content>
+      <v-form v-model="isValid">
+        <user-form-name />
+        <user-form-email />
+        <user-form-password />
+        <v-btn :disabled="!isValid" block class="white--text" color="appblue">
+          登録する
+        </v-btn>
+      </v-form>
+    </template>
+  </user-form-card>
 </template>
 
 <script>
 export default {
   // ログイン前なのでレイアウトを分ける
-  layout: 'before-login'
+  layout: 'before-login',
+  data () {
+    return {
+      isValid: false
+    }
+  }
 }
 </script>
