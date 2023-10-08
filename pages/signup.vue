37c9c2fe45d853766a57273b13a6ac02f96cf85b
takato hori <iuyda@icloud.com>
2023-10-08 17:56:29 +0900

    set data binding for signup.vue

--- pages/signup.vue
+++ pages/signup.vue
@@ -2,9 +2,9 @@
   <user-form-card>
     <template #user-form-card-content>
       <v-form v-model="isValid">
-        <user-form-name />
-        <user-form-email />
-        <user-form-password />
+        <user-form-name :name.sync="params.user.name" />
+        <user-form-email :email.sync="params.user.email" />
+        <user-form-password :password.sync="params.user.password" />
         <v-btn :disabled="!isValid" block class="white--text" color="appblue">
           登録する
         </v-btn>
@@ -19,7 +19,8 @@ export default {
   layout: 'before-login',
   data () {
     return {
-      isValid: false
+      isValid: false,
+      params: { user: { name: '', email: '', password: '' } }
     }
   }
 }
