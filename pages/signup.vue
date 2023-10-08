ae3c40709e3402c8dcb76b6d9b6045dad59097c8
takato hori <iuyda@icloud.com>
2023-10-08 19:46:56 +0900

    finished before login layouts

--- pages/signup.vue
+++ pages/signup.vue
@@ -1,11 +1,18 @@
 <template>
   <user-form-card>
     <template #user-form-card-content>
-      <v-form v-model="isValid">
+      <v-form ref="form" v-model="isValid">
         <user-form-name :name.sync="params.user.name" />
         <user-form-email :email.sync="params.user.email" placeholder />
         <user-form-password :password.sync="params.user.password" set-validation />
-        <v-btn :disabled="!isValid" block class="white--text" color="appblue">
+        <v-btn
+          :disabled="!isValid || loading"
+          :loading="loading"
+          block
+          class="white--text"
+          color="appblue"
+          @click="signup"
+        >
           登録する
         </v-btn>
       </v-form>
@@ -20,7 +27,23 @@ export default {
   data () {
     return {
       isValid: false,
+      loading: false,
       params: { user: { name: '', email: '', password: '' } }
+    }
+  },
+  methods: {
+    signup () {
+      this.loading = true
+      setTimeout(() => {
+        this.formReset()
+        this.loading = false
+      }, 1500)
+    },
+    formReset () {
+      this.$refs.form.reset()
+      for (const key in this.params.user) {
+        this.params.user[key] = ''
+      }
     }
   }
 }
