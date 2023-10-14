28181a1ed9de85ae1fb28cdcfd65848242e81171
takato hori <iuyda@icloud.com>
2023-10-14 21:36:05 +0900

    add projects.vue layout

--- plugins/my-inject.js
+++ plugins/my-inject.js
@@ -10,6 +10,18 @@ class MyInject {
     const title = this.app.i18n.t(jsonPath)
     return title
   }
+
+  dateFormat (dateStr) {
+    const dateTimeFormat = new Intl.DateTimeFormat(
+      'ja', { dateStyle: 'medium', timeStyle: 'short' }
+    )
+    return dateTimeFormat.format(new Date(dateStr))
+  }
+
+  // プロジェクトリンク
+  projectLinkTo (id, name = 'project-id-dashboard') {
+    return { name, params: { id } }
+  }
 }
 
 // inject => オリジナルクラスを追加できる
--- store/index.js
+++ store/index.js
@@ -2,7 +2,7 @@
 // アプリ全体で共有される
 // 記述方法は決まっているため、以下以外の書き方はできない
 
-const redirectPath = 'projects'
+const homePath = 'projects'
 
 export const state = () => ({
   // 追加
@@ -10,9 +10,19 @@ export const state = () => ({
     homeAppBarHeight: 56
   },
   loggedIn: {
-    redirectPath: {
-      name: redirectPath
+    homePath: {
+      name: homePath
     }
+  },
+  projects: {
+    current: null,
+    list: [
+      { id: 1, name: 'MyProject01', updatedAt: '2020-04-01T12:00:00+09:00' },
+      { id: 2, name: 'MyProject02', updatedAt: '2020-04-05T12:00:00+09:00' },
+      { id: 3, name: 'MyProject03', updatedAt: '2020-04-03T12:00:00+09:00' },
+      { id: 4, name: 'MyProject04', updatedAt: '2020-04-04T12:00:00+09:00' },
+      { id: 5, name: 'MyProject05', updatedAt: '2020-04-01T12:00:00+09:00' }
+    ]
   }
 })
 
