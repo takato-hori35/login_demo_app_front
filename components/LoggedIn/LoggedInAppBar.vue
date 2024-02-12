12749f06db1284b4a4187272c85477bb1e2ab01c
takato hori <iuyda@icloud.com>
2024-02-12 16:01:49 +0900

    Add breadCrumbs to LoggedInAppBar.vue

--- components/LoggedIn/LoggedInAppBar.vue
+++ components/LoggedIn/LoggedInAppBar.vue
@@ -14,10 +14,19 @@
       <app-logo />
     </nuxt-link>
 
-    <app-title />
+    <!-- アプリタイトル -->
+    <app-title
+      :class="{ 'hidden-mobile-and-down': isNotHomePath }"
+    />
+
+    <!-- パンくずリスト -->
+    <logged-in-app-bar-breadcrumbs
+      v-if="isNotHomePath"
+    />
 
     <v-spacer />
 
+    <!-- アカウントメニュー -->
     <logged-in-app-bar-account-menu />
   </v-app-bar>
 </template>
@@ -33,6 +42,11 @@ export default {
   data ({ $store }) {
     return {
       homePath: $store.state.loggedIn.homePath
+    }
+  },
+  computed: {
+    isNotHomePath () {
+      return this.$route.name !== this.homePath.name
     }
   }
 }
